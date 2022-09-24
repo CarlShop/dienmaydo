9d562f3e2ee0c10d882773beb410f351fe82748e
carlduy2002 <duydhgcc200018@fpt.edu.vn>
2022-09-24 10:26:53 +0700

    b

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 	
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-	$Connect = pg_connect("postgres://jmdrhuyxeteupz:59d17582c5cf3f93d4c70701169649492bf1ca1c26e7be4c1fed36ee1c91e358@ec2-34-205-14-168.compute-1.amazonaws.com:5432/d2hk0ut4ifpeo7");
+	$Connect = pg_connect("postgres://zwicraqyvdahlk:ac413e4e4b67b10b13f635cabbaf0674d62315a776950f1ec04ab4781649ec59@ec2-3-93-206-109.compute-1.amazonaws.com:5432/dbp5dc5hhhfgra");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	//postgres://zwicraqyvdahlk:ac413e4e4b67b10b13f635cabbaf0674d62315a776950f1ec04ab4781649ec59@ec2-3-93-206-109.compute-1.amazonaws.com:5432/dbp5dc5hhhfgra
